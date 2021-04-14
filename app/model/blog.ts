280ba9df695188eaa5d7796f593612b101f74cb6
RamKumarNonia <ramnonia33@gmail.com>
2021-04-15 02:12:50 +0530

    first commit

---
+++ app/model/blog.ts
@@ -0,0 +1,14 @@
+export class Blog {
+    
+    blogId!:number;
+    blogTitle!:String;
+    blogContent!:String;
+    blogPosted!:String;
+    status!:String;
+    noOflikes!:number;
+    noOfviews!:number;
+    noOfcomments!:number;
+    userId!:number;
+    username!:number;
+    
+}
